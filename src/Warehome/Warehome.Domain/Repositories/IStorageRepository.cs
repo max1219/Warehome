3a95911ab75dfed55056a13463e7e4a27c3fc7e1
max1219 <bescenniy.2003@mail.ru>
2026-02-10 13:35:07 +0600

    Add IStorageRepository

---
+++ src/Warehome/Warehome.Domain/Repositories/IStorageRepository.cs
@@ -0,0 +1,12 @@
+﻿using Warehome.Domain.Entities;
+
+namespace Warehome.Application.Repositories;
+
+public interface IStorageRepository
+{
+    Task<Storage?> GetByPathAsync(string path);
+    Task<IEnumerable<Storage>> GetAllAsync();
+    Task<bool> TryAddAsync(Storage storage);
+    Task<bool> DeleteAsync(Storage storage);
+    
+}
