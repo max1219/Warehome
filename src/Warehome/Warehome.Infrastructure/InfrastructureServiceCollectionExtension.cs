33938db5bd674f9509eec69e9bdfa1230683db20
max1219 <bescenniy.2003@mail.ru>
2026-02-23 18:41:16 +0600

    Add DI extensions

---
+++ src/Warehome/Warehome.Infrastructure/InfrastructureServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Warehome.Application.Repositories;
+using Warehome.Domain.Entities;
+using Warehome.Infrastructure.Data;
+using Warehome.Infrastructure.Data.Repositories;
+
+namespace Warehome.Infrastructure;
+
+public static class InfrastructureServiceCollectionExtension
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlite(configuration.GetConnectionString("Default")));
+        services.AddScoped<IStorageRepository, EfStorageRepository>();
+        services.AddScoped<ICategoryRepository<Storage>, EfStorageCategoryRepository>();
+        return services;
+    }
+}
