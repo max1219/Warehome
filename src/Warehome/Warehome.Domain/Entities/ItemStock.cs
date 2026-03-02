3d0b096c5d7d762a3575a7faba42a75484e616a1
max1219 <bescenniy.2003@mail.ru>
2026-03-02 16:34:19 +0600

    Add Storage and Id in domain ItemStock

--- src/Warehome/Warehome.Domain/Entities/ItemStock.cs
+++ src/Warehome/Warehome.Domain/Entities/ItemStock.cs
@@ -2,6 +2,8 @@
 
 public class ItemStock
 {
+    public int Id { get; set; }
     public ItemType ItemType { get; set; }
+    public Storage Storage { get; set; }
     public int Quantity { get; set; }
 }
